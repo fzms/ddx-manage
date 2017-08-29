2690aa0757b094ce1b82e0412fedd3142b757afe
Administrator <766710615@qq.com>
2017-08-29 17:46:59 +0800

    2017.08.28 index:nav

--- src/main/webapp/WEB-INF/views/js/navController.js
+++ src/main/webapp/WEB-INF/views/js/navController.js
@@ -2,41 +2,99 @@
  * Created by cxs on 2017/8/28.
  */
 var navApp=angular.module("navApp",[]);
-navApp.controller("navController",function($scope){
-    $scope.navList=[
+navApp.controller("navController",['$scope','$rootScope','$http','$location', function($scope,$http) {
+    $scope.navList = [
         {
-            "name":"产品管理","url":"a","icon":"fa-calendar","flag":"1",
-
-
-        },
-        {
-            "name":"仓库管理","url":"a","icon":"fa-home","flag":"2",
-            "subNav":[
+            "name": "产品管理", "url": "a", "icon": "fa-calendar", "flag": "1",
+            "subNav": [
                 {
-                    "name":"原料管理","url":"a","icon":"fa-envira"
+                    "name": "原料管理", "url": "a", "icon": "fa-envira"
                 },
                 {
-                    "name":"配件管理","url":"a","icon":"fa-unlock"
+                    "name": "配件管理", "url": "a", "icon": "fa-unlock"
                 },
                 {
-                    "name":"工具管理","url":"a","icon":"fa-wrench"
+                    "name": "工具管理", "url": "a", "icon": "fa-wrench"
                 },
                 {
-                    "name":"机器管理","url":"a","icon":"fa-rocket"
+                    "name": "机器管理", "url": "a", "icon": "fa-rocket"
                 }
             ]
         },
         {
-            "name":"订单管理","url":"a","icon":"fa-gavel","flag":"3"
+            "name": "仓库管理", "url": "a", "icon": "fa-home", "flag": "2",
+            "subNav": [
+                {
+                    "name": "原料管理", "url": "a", "icon": "fa-envira"
+                },
+                {
+                    "name": "配件管理", "url": "a", "icon": "fa-unlock"
+                },
+                {
+                    "name": "工具管理", "url": "a", "icon": "fa-wrench"
+                },
+                {
+                    "name": "机器管理", "url": "a", "icon": "fa-rocket"
+                }
+            ]
         },
         {
-            "name":"客户管理","url":"a","icon":"fa-handshake-o","flag":"4"
+            "name": "订单管理", "url": "a", "icon": "fa-gavel", "flag": "3"
         },
         {
-            "name":"员工管理","url":"a","icon":"fa-user","flag":"5"
+            "name": "客户管理", "url": "a", "icon": "fa-handshake-o", "flag": "4"
         },
         {
-            "name":"系统管理","url":"a","icon":"fa-gears","flag":"6"
+            "name": "员工管理", "url": "a", "icon": "fa-user", "flag": "5"
+        },
+        {
+            "name": "系统管理", "url": "a", "icon": "fa-gears", "flag": "6",
+            "subNav": [
+                {
+                    "name": "账号管理", "url": "a", "icon": "fa-fort-awesome"
+                },
+                {
+                    "name": "导航管理", "url": "a", "icon": "fa-flag"
+                }
+            ]
         }
     ];
-});
+    //第一种写法
+    $scope.actionMark={a:$scope.navList[0].name,b:$scope.navList[0].subNav[0] ? $scope.navList[0].subNav[0].name:""};
+    console.log($scope.actionMark);
+
+    // 第二种写法,后面navMenuActive加上判断
+    // $scope.actionMark=undefined;
+
+    $scope.changeNavMenu = function (a,b){
+        // if(b===undefined){
+        //
+        //     // b=$scope.actionMark.find(m => m.name===a).subNav[0].name;
+        //
+        //     b=$scope.navList.find(function(m){
+        //         return m.name===a;
+        //     }).subNav[0].name;
+        // };
+        $scope.actionMark={
+            a:a,
+            b:b
+        };
+        console.log($scope.actionMark);
+    };
+    $scope.navMenuActive = function (a,b){
+
+        // 第二种写法
+        // if($scope.actionMark===undefined){
+        //     return false;
+        // }
+
+        if(b===undefined){
+            return $scope.actionMark.a===a;
+        }else{
+            return $scope.actionMark.a===a &&  $scope.actionMark.b===b;
+        }
+
+    };
+
+}]
+);
