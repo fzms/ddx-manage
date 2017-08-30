8596d22509bba57985fb1fecd56a230915d2c7ad
Administrator <766710615@qq.com>
2017-08-30 16:30:44 +0800

    2017.08.30 index:tpls success

--- src/main/webapp/WEB-INF/views/js/mainApp.js
+++ src/main/webapp/WEB-INF/views/js/mainApp.js
@@ -2,12 +2,12 @@
  * Created by cxs on 2017/8/30.
  */
 var mainApp = angular.module("mainApp", [
-    "ui.router",
-    "ui.bootstrap",
-    "oc.lazyLoad",
-    "ngSanitize",
-    "ngAnimate",
-    "toaster"
+    // "ui.router",
+    // "ui.bootstrap",
+    // "oc.lazyLoad",
+    // "ngSanitize",
+    // "ngAnimate",
+    // "toaster"
 ]);
 mainApp.controller('appController', ['$scope', '$rootScope', function($scope, $rootScope) {
     // $scope.$on('$viewContentLoaded', function() {});
