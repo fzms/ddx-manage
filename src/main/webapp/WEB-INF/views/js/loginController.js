f53674154a527e34487f29f61694a2761a0d3f99
Administrator <766710615@qq.com>
2017-09-07 15:33:23 +0800

    2017.09.06  main header change

--- src/main/webapp/WEB-INF/views/js/loginController.js
+++ src/main/webapp/WEB-INF/views/js/loginController.js
@@ -3,25 +3,33 @@
  */
 var loginApp=angular.module("login",[]);
 loginApp.controller("loginController",function($scope,$http){
-    //获取验证码
+    //刷新验证码
     $scope.kaptcha=function(ev){
         console.log("success:"+ev.target.src);
-        $http.get("/kaptcha/generatecode").then(function(){
-            $('#kaptchaImage').hide().attr('src', '/kaptcha/generatecode').fadeIn();
-        },function(){
-            console.log("error");
-        });
+        $('#kaptchaImage').hide().attr('src', '/kaptcha/generatecode?' + Math.floor(Math.random() * 100)).fadeIn();
+        console.log("success:"+ev.target.src);
     };
 
     //点击提交
-    $scope.submitForm=function(){
-        console.log("hahah");
-        if($scope.loginData.$valid){
-            console.log($scope.loginData.username);
-            alert("提交成功！");
+    $scope.submitForm=function(data){
+        if($scope.loginData.$valid) {
+            $http.post("/login",$scope.loginData).then(function (data) {
+                console.log("success");
+            }, function (data) {
+                console.log("login error");
+                console.log($scope.loginData);
+            });
         }else{
-            alert("请确认登录用户名是否填写正确");
-            console.log($scope.loginData.username);
-        }
+            console.log($scope.loginData.loginName);
+        };
     };
 });
+//
+// console.log("hahah");
+// if($scope.loginData.$valid){
+//     console.log($scope.loginData.username);
+//     alert("提交成功！");
+// }else{
+//     alert("请确认登录用户名是否填写正确");
+//     console.log($scope.loginData.username);
+// }
