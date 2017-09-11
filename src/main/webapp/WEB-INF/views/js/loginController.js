5625679dc7b2c8e876750da9bc01a6e1ef94466a
Administrator <766710615@qq.com>
2017-09-11 15:52:07 +0800

    2017.09.11 login

--- src/main/webapp/WEB-INF/views/js/loginController.js
+++ src/main/webapp/WEB-INF/views/js/loginController.js
@@ -5,9 +5,8 @@ var loginApp=angular.module("login",[]);
 loginApp.controller("loginController",function($scope,$http){
     //刷新验证码
     $scope.kaptcha=function(ev){
-        console.log("success:"+ev.target.src);
+        // console.log("success:"+ev.target.src);
         $('#kaptchaImage').hide().attr('src', '/kaptcha/generatecode?' + Math.floor(Math.random() * 100)).fadeIn();
-        console.log("success:"+ev.target.src);
     };
 
     //点击提交
@@ -19,12 +18,10 @@ loginApp.controller("loginController",function($scope,$http){
             $http.post("/login",$scope.login).then(function (data) {
                 $scope.loginMessage=data.data;
                 if($scope.loginMessage.status==="success"){
-                    console.log(data.data.data["CLIENT_TOKEN"]);
-                    console.log(data.data.data["SESSION_USER_INFO"].realName);
-
+                    console.log($scope.loginMessage.data["CLIENT_TOKEN"]);
+                    console.log($scope.loginMessage.data["SESSION_USER_INFO"].realName);
                     console.log($scope.loginMessage.msg);
-                    // location.href="/index?name="+$scope.login.loginName;
-                    location.href="/index?CLIENT_TOKEN="+data.data.data["CLIENT_TOKEN"];
+                    location.href="/index?CLIENT_TOKEN="+$scope.loginMessage.data["CLIENT_TOKEN"];
                 }else{
                     $('#kaptchaImage').hide().attr('src', '/kaptcha/generatecode?' + Math.floor(Math.random() * 100)).fadeIn();
                 }
