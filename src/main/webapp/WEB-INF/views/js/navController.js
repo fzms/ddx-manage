6da73a3bd7d683853cea805a73cf678f630b6651
Administrator <766710615@qq.com>
2017-09-12 10:08:26 +0800

    2017.09.12  nav power

--- src/main/webapp/WEB-INF/views/js/navController.js
+++ src/main/webapp/WEB-INF/views/js/navController.js
@@ -10,12 +10,19 @@ app.controller("navController", function($scope,$http,$rootScope,navService,crum
         // console.log("showNav:"+$scope.showNav);
     });
 
+    //导航的权限管理，当flag为1的时候显示，当flag为0的时候不显示
     $scope.navList = [
+        {
+            "name": "a管理", "url": "/product", "icon": "fa-calendar", "flag": "0"
+        },
+        {
+            "name": "b管理", "url": "/product", "icon": "fa-calendar", "flag": "0"
+        },
         {
             "name": "产品管理", "url": "/product", "icon": "fa-calendar", "flag": "1"
         },
         {
-            "name": "仓库管理", "url": "/warehouse", "icon": "fa-home", "flag": "2",
+            "name": "仓库管理", "url": "/warehouse", "icon": "fa-home", "flag": "1",
             "subNav": [
                 {
                     "name": "原料管理", "url": "/warehouse/material", "icon": "fa-envira"
@@ -32,16 +39,16 @@ app.controller("navController", function($scope,$http,$rootScope,navService,crum
             ]
         },
         {
-            "name": "订单管理", "url": "/order", "icon": "fa-gavel", "flag": "3"
+            "name": "订单管理", "url": "/order", "icon": "fa-gavel", "flag": "1"
         },
         {
-            "name": "客户管理", "url": "/customer", "icon": "fa-handshake-o", "flag": "4"
+            "name": "客户管理", "url": "/customer", "icon": "fa-handshake-o", "flag": "1"
         },
         {
-            "name": "员工管理", "url": "/staff", "icon": "fa-user", "flag": "5"
+            "name": "员工管理", "url": "/staff", "icon": "fa-user", "flag": "1"
         },
         {
-            "name": "系统管理", "url": "/system", "icon": "fa-gears", "flag": "6",
+            "name": "系统管理", "url": "/system", "icon": "fa-gears", "flag": "1",
             "subNav": [
                 {
                     "name": "账号管理", "url": "/system/account", "icon": "fa-fort-awesome"
