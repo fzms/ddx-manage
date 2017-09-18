c9fd6e994d22530903aa654cc81126ae70020bd0
zhangmengwen <670609976@qq.com>
2017-09-18 13:52:44 +0800

    前端路径调整

--- src/main/java/com/ddx/manage/system/controller/IndexController.java
+++ src/main/java/com/ddx/manage/system/controller/IndexController.java
@@ -1,20 +1,17 @@
 package com.ddx.manage.system.controller;
 
-import com.ddx.manage.common.annotation.IgnoreSecurity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Administrator on 2017/9/7.
  */
 @Controller
 public class IndexController {
-    @IgnoreSecurity
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String login()throws Exception{
-        return "login";
-    }
+    //@IgnoreSecurity
+    //@RequestMapping(value = "/", method = RequestMethod.GET)
+    //public String login()throws Exception{
+    //    return "login";
+    //}
 
     //@IgnoreSecurity
     //@RequestMapping(value = "/index", method = RequestMethod.GET)
